81d00cabd0be525c0fb4ead1155a0aca40b4c076
Suit Claw <suitandclaw@Suits-MacBook-Pro.local>
2026-04-18 20:35:17 -0400

    test: v3 marker + testnet PIONEER_KEY

--- packages/abci-server/src/application.ts
+++ packages/abci-server/src/application.ts
@@ -258,7 +258,7 @@ interface CompletedUpgrade {
 }
 
 // Pioneer key DID (governance authority for upgrade proposals and Pioneer delegations)
-const PIONEER_KEY = "did:key:z6MknbJNu73PmSqtMxLwLfczCijQVphAMFuGXjyJFTaqfu3z";
+const PIONEER_KEY = "did:key:z6Mktm7VkikyjRLYERUwgbfmsqkrB2Evdh4AZBXurPXQsNQD";
 
 // Protocol Treasury genesis account (source of Pioneer delegation funds)
 const PROTOCOL_TREASURY_GENESIS = "did:key:z6Mki9jwpYMBB93zxYfsmNUHThpSgKATqydN4xJA1xcxGecm";
